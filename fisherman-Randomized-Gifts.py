06b24c827b3dc85cf7924ba43f99a3525f15b01a
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2023-12-06 11:34:41 +0330

    Randomized gifts for fisherman

---
+++ fisherman-Randomized-Gifts.py
@@ -0,0 +1,56 @@
+## Mohsen Pourdehghan
+
+import random
+
+giftList= ['fishing rod', 'fishing boat', 'fishing hat', 'fishing boots']
+randGift= random.choice(giftList)
+randFish= random.randint(250, 800)
+
+print('\n(You): Hello fisherman, how many fish did you catch?')
+
+print(f'\n(Fisherman): Not bad, just {randFish} pound. Do you wanna buy?')
+
+buy= input('\nSay somthing! ("yes" or "no"): ')
+
+while buy !='yes' and buy !='no':
+    
+    buy= input('\nJUST "yes" or "no": ')
+
+
+if buy =='yes':
+
+    pound_to_kilo= round(randFish * 0.45,2)
+    
+    print('\n(You): yes! i want to buy in kilograms.')
+
+    print(f'\n(Fisherman): It can be {pound_to_kilo} kilos. $2 per kilo, how many kilos do you want?')
+    
+    kilosNeed= float(input(f'\nSay somthing! (from 1 and {pound_to_kilo}): '))
+
+    # When the input is invalid 
+    while kilosNeed > pound_to_kilo or kilosNeed < 1 :
+        
+        kilosNeed= int(input(f'\nBETWEEN 1 AND {pound_to_kilo} !: '))
+    
+    money= kilosNeed * 2
+
+    #if over $300: gift one rod      
+    if money >= 300:
+        
+        print(f'\n(You): I need {kilosNeed}. here is your money ${money}. and this is your gift! a new {randGift}!!!')
+    
+        print('\n(Fisherman): Oh, Wow. Thanks!')
+
+    else:
+        
+        print(f'\n(You): I need {kilosNeed}. here is your money ${money}.')
+    
+        print('\n(Fisherman): Thanks, enjoy it!')
+
+elif buy =='no' :
+    
+    print('\n(You): No thanks.')
+    
+    print('\n(Fisherman): Ok.')
+
+
